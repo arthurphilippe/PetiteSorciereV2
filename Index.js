86e2140ca9c488900647b401f3d5ff516e38f9cf
Aloïs <alois@localhost.localdomain>
2019-01-14 11:15:24 +0100

    creating help module

--- Index.js
+++ Index.js
@@ -1,3 +1,14 @@
-bot.on();
-console.log("oui bonjour");
-document.write("oui bonjour");
+//Modules
+const fs = require('fs');
+
+//Global variables
+let token = fs.readFileSync("Token_PetiteSorciere.txt", "UTF-8")  //process.env.token; 
+
+bot.login(token);
+
+bot.on("message", message => {
+    //import
+    const help = require("./commands/help.js");
+    help.run(message);
+
+});
